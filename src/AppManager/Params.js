6974d9dca8b500a08b39883f78f33561736c733c
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-11 15:49:35 +0300

    change date

--- src/AppManager/Params.js
+++ src/AppManager/Params.js
@@ -1,5 +1,5 @@
 export const keyPush = 'fae1d907-c5ed-43e5-91a1-475552533d20';
 export const appID = 'id6744167006';
 export const keyApps = 'T9h7tLijQjDdZXpvgxh44L';
-export const targetDate = '2025-04-13';
+export const targetDate = '2025-04-18';
 export const bodyLin = 'https://mindblowing-dominant-championship.space/YOZ7XaVp';
